9a273928a82a37be4bf1e80f957b19f82150e8b2
Unknown <estuardo19901@hotmail.com>
2018-06-10 17:02:54 -0600

    Carpeta del Proyecto
    Aqui estan todos los archivos de la carpeta creada por Visual Studio.

---
+++ Programa/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+//Se debe de agregar lo que hicieron en el void con su nombre, se trabajaran variables locales asi que incluyanlo solo dentro del void correspondiente.
+
+void Estuardo() {
+	//Edwin Estuardo Barrios Garcia | 0901-17-103
+}
+
+void Steven() {
+
+}
+
+void Patrick() {
+
+}
+
+void David() {
+
+}
+int main() {
+
+}
